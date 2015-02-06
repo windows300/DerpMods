7ca397501d3bf63bced5d6026a5bc714676287b6
windows300 <russrulz@live.com>
2015-02-05 20:04:30 -0800

    Half way added a Test Ore.
    Need to add the stuff to texture it. It's 2/3 writing JSON files now.
    That will be fun :)

---
+++ com/github/derpmods/core/BlockTestOre.java
@@ -0,0 +1,31 @@
+package com.github.derpmods.core;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class BlockTestOre extends Block {
+
+	// This is not what the user see's as the name. It's what Minecraft see's it as
+	public final String name = "testOre";
+	
+	BlockTestOre() {
+		
+		// Basicly tell Minecraft that is like Stone.
+		super(Material.rock);
+		
+		/* Behind the scene's name. Make sure it's unique
+		 * because errors happen when you have two blocks
+		 * nammed the same.
+		*/
+		this.setUnlocalizedName(this.name);
+		
+		// Register this block with Minecraft.
+		GameRegistry.registerBlock(this, this.name);
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+}
--- com/github/derpmods/core/ModDerpMods.java
+++ com/github/derpmods/core/ModDerpMods.java
@@ -1,5 +1,6 @@
 package com.github.derpmods.core;
 
+import net.minecraft.block.Block;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -10,11 +11,23 @@ public class ModDerpMods {
 	public static final String MODID = "derpmods";
 	public static final String VERSION = "0.1a";
 	
+	public static Block testOre;
+	
 	@EventHandler
 	public void preinit(FMLInitializationEvent event) {
 		
-		// This is PreInit. Register Blocks and Items here. Also anything needing to be checked like
-		// Dependencies should be done here 
+		/* It's best practice to register blocks in preinit.
+		 * The way we set this up is so that all we have to do
+		 * is call and store a new version of BlockTestOre into
+		 * a local variable in this class. NEVER REFRENCE
+		 * BlockTestOre but here. Use the variable we store here.
+		*/
+		testOre = new BlockTestOre();
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
 		
 	}
 	
